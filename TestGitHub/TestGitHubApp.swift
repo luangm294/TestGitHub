24c830f36acfa3a14d6df47f8de19da95cc5d270
luangm294 <luan2008guilherme@gmail.com>
2023-05-30 09:28:18 -0400

    Initial Commit

---
+++ TestGitHub/TestGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitHubApp.swift
+//  TestGitHub
+//
+//  Created by projeto smart on 30/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TestGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
